405d9ced049387640d118464cd780d956fe70266
Kajal <kajal@krishaweb.net>
2016-06-29 13:53:25 +0530

    created at changes

--- app/models/device_report.rb
+++ app/models/device_report.rb
@@ -9,6 +9,8 @@ class DeviceReport
   column(:id, header: "Id", :order => "devices.id")
   column(:uuid, header: "Uuid", :order => "devices.uuid", class: "padding_class")
   column(:registration_id, header: "Registration Id", order: false, :order => "devices.registration_id", class: "padding_class")
+  column(:created_at, header: "Created At", :order => "devices.created_at")  
+  column(:updated_at, header: "Updated At", :order => "devices.updated_at")  
   column(:actions, header: "Actions", html: true , class: "padding_class" ) do |user|
     html = link_to "", admin_user_path(user), class: "btn btn-danger btn-xs glyphicon glyphicon-remove", method: :delete, title: "Remove User", 'data-confirm' => 'Are you sure?'
     html
