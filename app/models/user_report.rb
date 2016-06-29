af83733e6df1d8605131f11688d56145b06f1a56
Kajal <kajal@krishaweb.net>
2016-06-29 12:01:53 +0530

    changes

--- app/models/user_report.rb
+++ app/models/user_report.rb
@@ -10,12 +10,14 @@ class UserReport
   filter(:username, :string, header: "Username") {|value| where("username ilike ?", "%#{value}%")}
   filter(:email, :string, header: "E-mail") {|value| where("email ilike ?", "%#{value}%")}
 
-  column(:id, header: "Id", order: false)
-  column(:username, header: "Username", order: false)
-  column(:email, header: "E-mail", order: false)
+  column(:id, header: "Id", :order => "users.id")
+  column(:username, header: "Username", :order => "users.username", class: "padding_class")
+  column(:email, header: "E-mail", order: false, :order => "users.email", class: "padding_class")
+  column(:role, header: "Role", order: false, :order => "users.role", class: "padding_class")
   #column(:last_sign_in_at, html: true, header: "Laatste aanmelding") { |user| content_tag :span, time_ago_in_words(user.last_sign_in_at), title: user.last_sign_in_at.to_formatted_s(:long) if user.last_sign_in_at }
-  column(:actions, header: "Actions", html: true) do |user|
-    html = link_to "", edit_admin_user_path(user), class: "btn btn-default btn-xs glyphicon glyphicon-edit", title: "Edit User"
+  column(:actions, header: "Actions", html: true , class: "padding_class" ) do |user|
+    html = link_to "", admin_user_path(user), class: "btn btn-primary btn-xs glyphicon glyphicon-eye-open", title: "View User"
+    html += link_to "", edit_admin_user_path(user), class: "btn btn-default btn-xs glyphicon glyphicon-edit", title: "Edit User"
     html += link_to "", admin_user_path(user), class: "btn btn-danger btn-xs glyphicon glyphicon-remove", method: :delete, title: "Remove User", 'data-confirm' => 'Are you sure?'
     html
   end
