6b329d0c0c16e43c5cad9f7de4f85cf0ebd63320
Kajal <kajal@krishaweb.net>
2016-06-30 14:21:45 +0530

    add carrier wave for file upload

--- app/api/selfiecv_android.rb
+++ app/api/selfiecv_android.rb
@@ -190,7 +190,7 @@ class SelfiecvAndroid < Grape::API
       post :resume, jbuilder: 'all' do
         @user = User.find params[:user_id]
         @user.attributes = clean_params(params).permit(:title, :first_name,  :second_name, :last_name, :gender,  :date_of_birth, :nationality, :address, :city,  :contact_number,  :education_in,  :school_name, :year)
-        #@user.user_pic = params[:user_pic] if params[:user_pic]
+        @user.file = params[:file] if params[:file]
         error! @user.errors.full_messages.join(', '), 422 unless @user.save
         @user
       end
