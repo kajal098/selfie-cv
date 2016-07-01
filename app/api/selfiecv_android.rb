3ea22dbb0780ae485b07c48f116b6421d0044f65
Kajal <kajal@krishaweb.net>
2016-07-01 16:55:34 +0530

    changes

--- app/api/selfiecv_android.rb
+++ app/api/selfiecv_android.rb
@@ -39,7 +39,7 @@ class SelfiecvAndroid < Grape::API
       optional :registration_id, type: String
     end
     post :register do
-      status 200
+      render status: 200
       @device = Device.find_or_initialize_by uuid: params[:uuid]
       @device.registration_id = params[:registration_id]
       @device.renew_token
