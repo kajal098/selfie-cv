eabbb49cca5dfc28ae92328ebed514a01d32e6ea
Ravi <ravi@krishaweb.com>
2016-07-04 15:34:35 +0530

    solve 200 error

--- app/api/selfiecv_android.rb
+++ app/api/selfiecv_android.rb
@@ -7,9 +7,13 @@ class SelfiecvAndroid < Grape::API
   version 'android', using: :path
   format :json 
   formatter :json, Grape::Formatter::Jbuilder
-  rescue_from Grape::Exceptions::ValidationErrors do |e|
-    error!(e, 200)
+
+  # Send Validation Error with 200 status code
+  rescue_from :all do |e|
+    error!(e.message, 200)
   end
+
+  # Default status on 500 Error
   default_error_status 200
 
   helpers do
@@ -31,21 +35,6 @@ class SelfiecvAndroid < Grape::API
       error! 'Unauthorized', 401 unless current_user
     end
 
-    def api_response response
-      case response
-      when Integer
-        status response
-      when String
-        response
-      when Hash
-        response
-      when Net::HTTPResponse
-        "#{response.code}: #{response.message}"
-      else
-        status 200 # Bad request
-      end
-    end
-
   end
 
 # devices start
@@ -58,8 +47,7 @@ class SelfiecvAndroid < Grape::API
       optional :registration_id, type: String
     end
     post :register do
-      status 200
-      @device = Device.find_by uuid: params[:uuid]
+      @device = Device.find_or_initialize_by uuid: params[:uuid]
       @device.registration_id = params[:registration_id]
       @device.renew_token
       error! @device.errors.full_messages.join(', '), 200 unless @device.save
