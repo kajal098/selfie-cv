72f429050f047e8618f321ec3f75df43b09d45fc
Kajal <kajal@krishaweb.net>
2016-07-04 16:47:25 +0530

    company model added

--- app/api/selfiecv_android.rb
+++ app/api/selfiecv_android.rb
@@ -384,7 +384,7 @@ class SelfiecvAndroid < Grape::API
             params do
               requires :token, type: String, regexp: UUID_REGEX
             end
-            get :company_and_spe, jbuilder: 'all' do
+            post :company_and_spe, jbuilder: 'all' do
               @courses = Course.all
               @specializations = Specialization.all
             end
