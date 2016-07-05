2889c891aa7556bf5583d0d783d2948d8323b4b9
Kajal <kajal@krishaweb.net>
2016-07-05 11:44:41 +0530

    generate model company galery

--- app/api/selfiecv_android.rb
+++ app/api/selfiecv_android.rb
@@ -406,7 +406,8 @@ class SelfiecvAndroid < Grape::API
             end
             post :company_galery, jbuilder: 'galery' do
               @user = User.find params[:user_id]
-              @user.file = params[:file] if params[:file]
+              @galery = CompanyGalery.new
+              @galery.file = params[:file] if params[:file]
             end
 
 
