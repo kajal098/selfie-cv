2c918b71f61c8c0acffb0b7130bea91127d52c37
Kajal <kajal@krishaweb.net>
2016-07-15 17:06:19 +0530

    changes

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,22 +1,20 @@
 class Admin::SettingsController < Admin::ApplicationController
-
-def show
-    @setting = Setting.new
+  def show
+    @setting = Setting.first
   end
-
   def create
-    @setting = Setting.new  
+    @setting = Setting.first
     if @setting.update params.require(:setting).permit!      
       redirect_to admin_settings_path, notice: "Settings updated successfully."
     else
       render action: :show
     end
   end
-
   def update
     self.create
   end
-
-  
+  private
+  def setting_params
+    params.require(:setting).permit(:resume_per, :achievement_per, :curricular_per, :whizquiz_per, :future_goal_per, :Working_env_per, :reference_per, :site_name, :site_email, :site_phone, :site_fax, :facebook_url, :twitter_url, :google_plus_url)
+  end
 end
-
