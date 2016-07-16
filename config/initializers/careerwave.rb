78603e499fe2b5d5e73ed838a414ebf065e4e678
Kajal <kajal@krishaweb.net>
2016-07-16 19:02:12 +0530

    changes

---
+++ config/initializers/careerwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage    = :aws
+    config.aws_bucket = ENV['S3_BUCKET_NAME']
+    config.aws_acl    = :public_read
+    config.aws_credentials = {
+      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    }
+  end
+end
