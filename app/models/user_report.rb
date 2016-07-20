0573309ed71ade8cc97d8dfb75e57818ec3c1199
Kajal <kajal@krishaweb.net>
2016-07-20 14:30:17 +0530

    set video tag

--- app/models/user_report.rb
+++ app/models/user_report.rb
@@ -16,8 +16,7 @@ class UserReport
     model.created_at.to_date
   end
   column(:file, :html => true, class: 'padding_class',) do |model|
-        image_tag model.file.url,  :size => "20x20"
-        video_tag model.file.url,  :size => "200x100"
+        video_tag model.file.url,  :size => "320x240"
       end
   column(:actions, header: "", html: true , class: "padding_class" ) do |user|
     html = link_to "", admin_user_path(user), class: "margin_class btn btn-primary btn-xs glyphicon glyphicon-eye-open", title: "View User"
