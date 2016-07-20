7beb24b8a0dc967b7b28b1db2147d16ccb701206
Kajal <kajal@krishaweb.net>
2016-07-20 12:48:24 +0530

    fix width

--- app/models/user_report.rb
+++ app/models/user_report.rb
@@ -11,10 +11,12 @@ class UserReport
   column(:role, header: "Role",  :order => "users.role", class: "padding_class")
   column(:username, header: "Username", :order => "users.username", class: "padding_class")
   column(:email, header: "E-mail",  :order => "users.email", class: "padding_class")
-  column(:file, header: "File",   class: "padding_class")
   column(:updated_at) do |model|
     model.created_at.to_date
   end
+  column(:file, :html => true, class: 'padding_class',) do |model|
+        image_tag model.file.url,  :size => "20x20"
+      end
   column(:actions, header: "", html: true , class: "padding_class" ) do |user|
     html = link_to "", admin_user_path(user), class: "margin_class btn btn-primary btn-xs glyphicon glyphicon-eye-open", title: "View User"
     html += link_to "", admin_user_path(user), class: "margin_class btn btn-danger btn-xs glyphicon glyphicon-remove", method: :delete, title: "Remove User", 'data-confirm' => 'Are you sure?'
