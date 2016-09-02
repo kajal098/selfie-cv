493a14c3f2361ff3d8b2006fa038658026b91a55
Kajal <kajal@krishaweb.net>
2016-09-02 12:41:22 +0530

    changes

--- db/migrate/20160829052358_create_student_educations.rb
+++ db/migrate/20160829052358_create_student_educations.rb
@@ -2,7 +2,7 @@ class CreateStudentEducations < ActiveRecord::Migration
   def change
     create_table :student_educations do |t|
       t.integer :user_id
-      t.string :standard
+      t.string :standard,              null: false, default: ""
       t.string :year,              null: false, default: ""
       t.string :school,              null: false, default: ""
       t.timestamps null: false
