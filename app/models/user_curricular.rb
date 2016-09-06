fd2798b655ec5bd15801999429e0a14d74c9a348
kajal <kajal@krishaweb.net>
2016-09-06 11:23:01 +0530

    ok

--- app/models/user_curricular.rb
+++ app/models/user_curricular.rb
@@ -5,12 +5,8 @@ class UserCurricular < ActiveRecord::Base
 	#validates_format_of :date, :with => /\d{2}\/\d{2}\/\d{4}/
 
 	mount_uploader :file, FileUploader
-    def thumb_url
-        if(file.identifier.blank?)
-            ActionController::Base.helpers.asset_url("curricular.png")
-        else    
-            file.url(:thumb)
-        end
+    def thumb_url  
+        file.url(:thumb)
     end
 
     def photo_url; file.url; end
