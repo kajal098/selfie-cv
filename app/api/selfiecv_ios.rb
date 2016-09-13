f1aa3c4b417c7f3278bb94d31b24ec606278c3fb
kajal <kajal@krishaweb.net>
2016-09-13 16:17:00 +0530

    chat listing in both

--- app/api/selfiecv_ios.rb
+++ app/api/selfiecv_ios.rb
@@ -1844,9 +1844,9 @@ resources :messages do
     params do
       requires :token, type: String, regexp: UUID_REGEX
       requires :group_id
-      requires :quick_msg
-      requires :file
-      requires :file_type
+      optional :quick_msg
+      optional :file
+      optional :file_type
     end
     post :create, jbuilder: 'ios_message' do
         @chat = Chat.new group_id: params[:group_id], sender_id: current_user.id, quick_msg: params[:quick_msg], file_type: params[:file_type]
