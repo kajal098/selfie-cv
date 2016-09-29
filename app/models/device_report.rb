b18136e48c25ce6fa61a082ca28c0ea59fd37774
kajal <kajal@krishaweb.net>
2016-09-29 14:37:34 +0530

    ok

--- app/models/device_report.rb
+++ app/models/device_report.rb
@@ -13,9 +13,10 @@ class DeviceReport
   column(:registration_id, header: "Registration Id", :order => "devices.registration_id")
   column(:updated_at, html: true, header: "Updated At") { |device| content_tag :span, time_ago_in_words(device.updated_at), title: device.updated_at.to_formatted_s(:long) if device.updated_at }
   
-  column(:actions, header: "Action", html: true , class: "margin_class padding_class" ) do |user|
+  column(:actions, header: "Action", html: true ) do |user|
     html = link_to "", admin_device_path(user), class: "margin_class btn btn-danger btn-xs glyphicon glyphicon-remove", method: :delete, title: "Remove User", 'data-confirm' => 'Are you sure?'
     html
   end
 
 end
+
