fd8652ac83e16b78e85ec336fed2ca7d297e257f
kajal <kajal@krishaweb.net>
2016-10-05 11:36:37 +0530

    changes in whizquiz

---
+++ app/views/api/android_whiz_quiz.jbuilder
@@ -0,0 +1,10 @@
+json.status "Success"
+
+if @questions
+	json.questions @questions do |question|
+		json.extract! question, :id, :question
+
+		json.created_at question.created_at.to_i
+		json.updated_at question.updated_at.to_i
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -80,8 +80,8 @@ ActiveRecord::Schema.define(version: 20161004072847) do
     t.string   "collage_name", default: "",           null: false
     t.string   "subject",      default: "",           null: false
     t.string   "designation",  default: "",           null: false
-    t.date     "join_from",    default: '2016-10-04'
-    t.date     "join_till",    default: '2016-10-04'
+    t.date     "join_from",    default: '2016-10-05'
+    t.date     "join_till",    default: '2016-10-05'
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
@@ -245,7 +245,7 @@ ActiveRecord::Schema.define(version: 20161004072847) do
     t.string   "title",           default: "",           null: false
     t.string   "team_type",       default: "",           null: false
     t.string   "location",        default: "",           null: false
-    t.date     "date",            default: '2016-10-04'
+    t.date     "date",            default: '2016-10-05'
     t.string   "file",            default: ""
     t.string   "text_field",      default: "",           null: false
     t.string   "file_type",       default: "",           null: false
@@ -290,7 +290,7 @@ ActiveRecord::Schema.define(version: 20161004072847) do
     t.integer  "user_id"
     t.string   "name",            default: "",           null: false
     t.string   "exp_type",        default: "",           null: false
-    t.date     "start_from",      default: '2016-10-04'
+    t.date     "start_from",      default: '2016-10-05'
     t.string   "working_till",    default: "",           null: false
     t.string   "designation",     default: "",           null: false
     t.string   "description",     default: "",           null: false
@@ -446,7 +446,7 @@ ActiveRecord::Schema.define(version: 20161004072847) do
     t.string   "from",       default: "",           null: false
     t.string   "email",      default: "",           null: false
     t.string   "contact",    default: "",           null: false
-    t.date     "date",       default: '2016-10-04'
+    t.date     "date",       default: '2016-10-05'
     t.string   "location",   default: "",           null: false
     t.string   "file",       default: ""
     t.string   "text_field", default: "",           null: false
@@ -499,7 +499,7 @@ ActiveRecord::Schema.define(version: 20161004072847) do
     t.string   "last_name",                                     default: "",           null: false
     t.string   "profile_pic",                                   default: ""
     t.string   "gender",                                        default: "",           null: false
-    t.string   "date_of_birth",                                 default: "2016-10-04"
+    t.string   "date_of_birth",                                 default: "2016-10-05"
     t.string   "nationality",                                   default: "",           null: false
     t.string   "address",                                       default: "",           null: false
     t.string   "city",                                          default: "",           null: false
@@ -513,7 +513,7 @@ ActiveRecord::Schema.define(version: 20161004072847) do
     t.string   "faculty_uni_name",                              default: "",           null: false
     t.string   "faculty_subject",                               default: "",           null: false
     t.string   "faculty_designation",                           default: "",           null: false
-    t.string   "faculty_join_from",                             default: "2016-10-04"
+    t.string   "faculty_join_from",                             default: "2016-10-05"
     t.string   "company_name",                                  default: "",           null: false
     t.string   "company_establish_from",                        default: "",           null: false
     t.integer  "industry_id"
