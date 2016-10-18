1f4959e5573c791ba53179061866b39f443dae70
kajal <kajal@krishaweb.net>
2016-10-18 17:03:49 +0530

    add category in admin panel

--- app/models/company_galery.rb
+++ app/models/company_galery.rb
@@ -9,21 +9,22 @@ class CompanyGalery < ActiveRecord::Base
   after_destroy :percent_of_company_galery
 
   	def percent_of_company_galery
-  		user = self.user
-        @count = user.company_galeries.count
+      user = self.user
         galery_per = 0
         setting_per = UserPercentage.find_by_key('gallery').value.to_i
-        if @count > 0  
-        	if @count >= 10 && @count <= 30
-	            @galery_per = setting_per * 0.3
-	        elsif @count >= 30 &&  @count <= 50
-	            @galery_per = setting_per * 0.5
-	        elsif @count >= 50
-	            @galery_per = setting_per * 1
-	        end
-            user.user_meter.update_column('galery_per' ,galery_per)
+        @count = user.company_galeries.count
+        if user.company_galeries.count > 0  
+          if @count >= 10 && @count <= 20
+              galery_per = setting_per * 0.3
+          elsif @count >= 20 &&  @count <= 30
+              galery_per = setting_per * 0.5
+          elsif @count >= 30
+              galery_per = setting_per * 1
+          end
         end 
+        user.user_meter.update_column('galery_per' ,galery_per)
+        user.profile_meter_total
         return true
-	end
+    end
 
 end
