97caddb70afae36a91774569c94a02aaa191e04d
kajal <kajal@krishaweb.net>
2016-10-21 11:42:47 +0530

    changes

--- app/api/selfiecv_ios.rb
+++ app/api/selfiecv_ios.rb
@@ -1972,7 +1972,7 @@ end
 post :view, jbuilder: 'ios_notification' do
 @user_view = UserView.new user_id: current_user.id, view_id: params[:view_id]
 error! @user_view.errors.full_messages.join(', '),422 unless @user_view.save
-Device.notify User.find(params[:view_id]).active_devices, { msg: "#{current_user.username} viewed your profile.", who_like_photo: current_user.file.url, name: current_user.username, time: Time.now, id: current_user.id }     
+#Device.notify User.find(params[:view_id]).active_devices, { msg: "#{current_user.username} viewed your profile.", who_like_photo: current_user.file.url, name: current_user.username, time: Time.now, id: current_user.id }     
 end
 
 desc 'Share Profile'
@@ -1984,7 +1984,7 @@ end
 post :share, jbuilder: 'ios_notification' do
 @user_share = UserShare.new user_id: current_user.id, share_id: params[:share_id], share_type: params[:share_type]
 error! @user_share.errors.full_messages.join(', '),422 unless @user_share.save
-Device.notify User.find(params[:share_id]).active_devices, { msg: "#{current_user.username} shared your profile on #{params[:share_type]}.", who_like_photo: current_user.file.url, name: current_user.username, time: Time.now, id: current_user.id }     
+#Device.notify User.find(params[:share_id]).active_devices, { msg: "#{current_user.username} shared your profile on #{params[:share_type]}.", who_like_photo: current_user.file.url, name: current_user.username, time: Time.now, id: current_user.id }     
 end
 
 desc 'Favourite Profile'
@@ -2012,7 +2012,7 @@ end
 post :rate, jbuilder: 'ios_notification' do
 @user_rate = UserRate.new user_id: current_user.id, rate_id: params[:rate_id], rate_type: params[:rate_type]
 error! @user_rate.errors.full_messages.join(', '),422 unless @user_rate.save     
-Device.notify User.find(params[:rate_id]).active_devices, { msg: "#{current_user.username} rate your profile.", who_like_photo: current_user.file.url, name: current_user.username, time: Time.now, id: current_user.id }
+#Device.notify User.find(params[:rate_id]).active_devices, { msg: "#{current_user.username} rate your profile.", who_like_photo: current_user.file.url, name: current_user.username, time: Time.now, id: current_user.id }
 end
 desc  "LIST Of NOTIFICATION"
 params  do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019085841) do
+ActiveRecord::Schema.define(version: 20161021054121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 20161019085841) do
   end
 
   add_index "company_galeries", ["user_id"], name: "index_company_galeries_on_user_id", using: :btree
+
+  create_table "company_stocks", force: :cascade do |t|
+    t.string   "country",    default: "", null: false
+    t.string   "sensex",     default: "", null: false
+    t.string   "currency",   default: "", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name",       default: "", null: false
