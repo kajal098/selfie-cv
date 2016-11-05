e9d5037e6754f1fe705a2e3f7744416fd3404342
kajal <kajal@krishaweb.net>
2016-11-05 16:24:50 +0530

    changes in notification

--- app/models/user_marketiq.rb
+++ app/models/user_marketiq.rb
@@ -1,4 +1,25 @@
 class UserMarketiq < ActiveRecord::Base
 
 	belongs_to :user
+
+	after_save :percent_of_marketiq
+    
+    def percent_of_marketiq()
+    	@count = user.user_marketiqs.where(status: true).count
+        marketiq_per = 0
+        setting_per = UserPercentage.find_by_key('marketIQ').value.to_i
+        if @count > 0  
+        	if @count >= 1 && @count <= 2
+	            marketiq_per = setting_per * 0.3
+	        elsif @count >= 3 &&  @count <= 5
+	            marketiq_per = setting_per * 0.5
+	        elsif @count >= 9
+	            marketiq_per = setting_per * 1
+	        end
+        end 
+		user.user_meter.update_column('marketiq_per' ,marketiq_per)
+        user.profile_meter_total
+        return true
+    end
+
 end
--- app/models/user_whizquiz.rb
+++ app/models/user_whizquiz.rb
@@ -10,10 +10,10 @@ class UserWhizquiz < ActiveRecord::Base
     def photo_url; review.url; end
 
     after_save :percent_of_whizquiz
-    before_destroy :percent_of_whizquiz
+    #before_destroy :reduce_percentage
 
     def percent_of_whizquiz()
-    	@count = user.user_whizquizzes.count
+    	@count = user.user_whizquizzes.where(status: true).count
         whizquiz_per = 0
         setting_per = UserPercentage.find_by_key('whizquiz').value.to_i
         if @count > 0  
@@ -29,6 +29,51 @@ class UserWhizquiz < ActiveRecord::Base
         user.profile_meter_total
         return true
     end
+
+    # def percent_of_whizquiz()
+    # 	user = self.user
+    #     whizquiz_per = 0
+    #     setting_per = UserPercentage.find_by_key('whizquiz').value.to_i
+    #     if user.user_whizquizzes.count > 0  
+    #     	user.user_whizquizzes.each do |whizquiz|   
+    #     	   		  if whizquiz.file_type == "video"
+	   #                  whizquiz_per = setting_per.value.to_i * 1
+	   #                  break
+	   #              elsif whizquiz.file_type == "image"
+	   #                  whizquiz_per = setting_per.value.to_i * 0.7
+	   #                  break
+	   #              else
+	   #                  whizquiz_per = setting_per.value.to_i * 0.5
+	   #              end        		       	
+    #     	end
+    #     end 
+    #     user.user_meter.update_column('whizquiz_per' ,whizquiz_per)
+    #     user.profile_meter_total
+    #     return true
+    # end
+
+
+    # def reduce_percentage
+    #     user = self.user
+    #     whizquiz_per = 0
+    #     if user.user_whizquizzes.where.not(id: self.id).count > 0 
+    #         setting_per = UserPercentage.find_by_key('whizquiz').value.to_i
+    #         user.user_whizquizzes.where.not(id: self.id).each do |whizquiz|   
+    #                 if whizquiz.file_type == "video"
+    #                     whizquiz_per = setting_per.value.to_i * 1
+    #                     break
+    #                 elsif whizquiz.file_type == "image"
+    #                     whizquiz_per = setting_per.value.to_i * 0.7
+    #                     break
+    #                 else
+    #                     whizquiz_per = setting_per.value.to_i * 0.5
+    #                 end
+    #         end
+    #     end
+    #     user.user_meter.update_column('whizquiz_per' ,whizquiz_per)
+    #     user.profile_meter_total
+    #     return true
+    # end
 
 
 end
