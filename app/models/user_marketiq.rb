dc1014b6e94d4238e1080982512606b974422a02
kajal <kajal@krishaweb.net>
2016-11-05 17:17:02 +0530

    changes in notification

--- app/models/user_marketiq.rb
+++ app/models/user_marketiq.rb
@@ -6,18 +6,18 @@ class UserMarketiq < ActiveRecord::Base
     
     def percent_of_marketiq()
     	@count = user.user_marketiqs.where(status: true).count
-        marketiq_per = 0
+        market_iq_per = 0
         setting_per = UserPercentage.find_by_key('marketIQ').value.to_i
         if @count > 0  
         	if @count >= 2 && @count <= 3
-	            marketiq_per = setting_per * 0.3
+	            market_iq_per = setting_per * 0.3
 	        elsif @count >= 3 &&  @count <= 5
-	            marketiq_per = setting_per * 0.5
+	            market_iq_per = setting_per * 0.5
 	        elsif @count >= 5
-	            marketiq_per = setting_per * 1
+	            market_iq_per = setting_per * 1
 	        end
         end 
-		user.user_meter.update_column('market_iq_per' ,marketiq_per)
+		user.user_meter.update_column('market_iq_per' ,market_iq_per)
         user.profile_meter_total
         return true
     end
