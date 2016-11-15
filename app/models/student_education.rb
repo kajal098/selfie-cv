34b0b4bb9eedae41930c87b27db0743e25e177f1
kajal <kajal@krishaweb.net>
2016-11-15 15:02:09 +0530

    new changes

--- app/models/student_education.rb
+++ app/models/student_education.rb
@@ -6,21 +6,20 @@ class StudentEducation < ActiveRecord::Base
 
 	def percent_of_education()
     	user = self.user
-        
+        student_education_info_per = 0
+        setting_per = UserPercentage.where(key: 'education').where(ptype: "Student").first
+
         if user.user_educations.count > 0  
-        	student_education_info_per = 0
-        	setting_per = UserPercentage.where(key: 'education').where(ptype: "Student").first
-        	user.user_educations.each do |education|   
-        	   		
-        	   		if education.standard.present?           	   		
-	                    workshop_per = setting_per.value.to_i * 1
-	                    break
-	                else
-	                    workshop_per = setting_per.value.to_i * 0.5
-	                end
-        		       	
-        	end
+            user.user_educations.each do |education|   
+                if education.standard.present?                      
+                    workshop_per = setting_per.value.to_i * 1
+                    break
+                else
+                    workshop_per = setting_per.value.to_i * 0.5
+                end
+            end
         end 
+        
         user.user_meter.update_column('student_education_info_per' ,student_education_info_per)
         user.profile_meter_total
         return true
