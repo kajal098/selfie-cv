387814ba9b553d526129d1bc6c4ffb38637afae4
kajal <kajal@krishaweb.net>
2016-11-22 16:53:50 +0530

    ok

--- app/models/video_upload_report.rb
+++ app/models/video_upload_report.rb
@@ -6,11 +6,11 @@ class VideoUploadReport
   
   column(:id, header: "id", :order => "video_uploads.id")
   column(:file, header: "File", :order => "video_uploads.file")
-  column(:updated_at, html: true, header: "Updated At") { |chat| content_tag :span, time_ago_in_words(chat.updated_at), title: chat.updated_at.to_formatted_s(:long) if chat.updated_at }
-  column(:actions, header: "Action", html: true  ) do |chat|
-    html = link_to "", admin_chat_path(chat), class: "margin_class btn btn-primary btn-xs glyphicon glyphicon-eye-open", title: "View chat"
-    html += link_to "", edit_admin_chat_path(chat), class: "margin_class btn btn-default btn-xs glyphicon glyphicon-edit", title: "Edit chat"
-    html += link_to "", admin_chat_path(chat), class: "margin_class btn btn-danger btn-xs glyphicon glyphicon-remove", method: :delete, title: "Remove chat", 'data-confirm' => 'Are you sure?'
+  column(:updated_at, html: true, header: "Updated At") { |video_upload| content_tag :span, time_ago_in_words(video_upload.updated_at), title: video_upload.updated_at.to_formatted_s(:long) if video_upload.updated_at }
+  column(:actions, header: "Action", html: true  ) do |video_upload|
+    html = link_to "", admin_video_upload_path(video_upload), class: "margin_class btn btn-primary btn-xs glyphicon glyphicon-eye-open", title: "View Video"
+    html += link_to "", edit_admin_video_upload_path(video_upload), class: "margin_class btn btn-default btn-xs glyphicon glyphicon-edit", title: "Edit Video"
+    html += link_to "", admin_video_upload_path(video_upload), class: "margin_class btn btn-danger btn-xs glyphicon glyphicon-remove", method: :delete, title: "Remove Video", 'data-confirm' => 'Are you sure?'
     html
   end
 
