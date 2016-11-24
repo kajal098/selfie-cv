583a4417fc8a8e534ab6d0eebda6837efa122740
kajal <kajal@krishaweb.net>
2016-11-24 18:50:41 +0530

    categories

--- db/migrate/20160627125155_create_company_stocks.rb
+++ db/migrate/20160627125155_create_company_stocks.rb
@@ -2,10 +2,13 @@ class CreateCompanyStocks < ActiveRecord::Migration
   def change
     create_table :company_stocks do |t|
     	t.string  :sensex_co,              null:false, default: ""
+    	t.integer :category_id
     	t.string  :sensex,              null:false, default: ""
     	t.string  :currency,              null:false, default: ""
     	t.string  :date_format,              null:false, default: "dd/mm/yyyy"
         t.timestamps null: false
     end
+    add_index :company_stocks, [:category_id]
+    add_foreign_key :company_stocks, :categories, on_delete: :cascade
   end
 end
