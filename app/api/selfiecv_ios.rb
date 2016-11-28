e258639ebdda9e2033b57fbb5ebb0c626b842f63
kajal <kajal@krishaweb.net>
2016-11-28 16:18:03 +0530

    ok

--- app/api/selfiecv_ios.rb
+++ app/api/selfiecv_ios.rb
@@ -1586,7 +1586,7 @@ before { authenticate! }
 	post :update_faculty_workshop, jbuilder: 'ios' do
 		@faculty_workshop = FacultyWorkshop.find params[:workshop_id]
 		error! 'Faculty workshop not found',422 unless @faculty_workshop
-		@faculty_workshop.attributes = clean_params(params).permit(:titl, :description, :file_type)
+		@faculty_workshop.attributes = clean_params(params).permit(:title, :description, :file_type)
 		@faculty_workshop.file = params[:file] if params[:file]
 		error! @faculty_workshop.errors.full_messages.join(', '),422 unless @faculty_workshop.save
 	end
