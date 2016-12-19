71fd9b8aeb8694f180a44ca9fabf6906881b39af
kajal <kajal@krishaweb.net>
2016-12-19 15:13:10 +0530

    db reset

--- app/api/selfiecv_ios.rb
+++ app/api/selfiecv_ios.rb
@@ -2017,22 +2017,22 @@ end
 resources :folder do
 	before { authenticate! }
 	desc 'Create Folder'
-	params do
-		requires :token, type: String, regexp: UUID_REGEX
-		requires :name
-	end
-	post :create, jbuilder: 'ios_folder' do
-		@a = UserFolder.joins(:folder).where("user_folders.user_id = ?", current_user.id).where('folders.name = ?', params[:name].downcase)
-	    if @a.count > 0
-	    	error! 'Folder name already exist! Please try another one!',422
-		else
-			@folder = Folder.new name: params[:name].downcase, default_status: false
-			error! @folder.errors.full_messages.join(', '),422 unless @folder.save
-			@user_folder = UserFolder.new user_id: current_user.id
-			@user_folder.folder_id = @folder.id
-			error! @user_folder.errors.full_messages.join(', '),422 unless @user_folder.save
-		end         
-	end
+  params do
+    requires :token, type: String, regexp: UUID_REGEX
+    requires :name
+  end
+  post :create, jbuilder: 'android_folder' do
+    @a = UserFolder.joins(:folder).where("user_folders.user_id = ?", current_user.id).where('folders.name = ?', params[:name].downcase).first
+    if @a
+      error! 'Folder name already exist! Please try another one!', 422
+    else
+      @folder = Folder.new name: params[:name].downcase, default_status: false
+      error! @folder.errors.full_messages.join(', '), 422 unless @folder.save
+      @user_folder = UserFolder.new user_id: current_user.id
+      @user_folder.folder_id = @folder.id
+      error! @user_folder.errors.full_messages.join(', '), 422 unless @user_folder.save
+    end          
+  end
 	desc 'List Folder'
 	params do
 		requires :token, type: String, regexp: UUID_REGEX
